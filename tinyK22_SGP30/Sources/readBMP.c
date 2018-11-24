b01bfb0b559263e3c26faffeb59520f28db30bfe
pescatore89 <andy.fischer@stud.hslu.ch>
2018-11-24 01:04:45 +0100

    Daten sind nun im data vom Image struct bereit und können verarbeitet werden

--- tinyK22_SGP30/Sources/readBMP.c
+++ tinyK22_SGP30/Sources/readBMP.c
@@ -96,31 +96,42 @@ uint8_t Read_readBMP(const uint8_t *fileName, const CLS1_StdIOType *io) {
 
 	uint8_t res = ERR_OK;
 	int result = 0;
-	BMPImage* img = NULL;
-	img = malloc(sizeof(BMPImage));
-	if (img != NULL) {
-		img->data = malloc(NUMBER_OF_LEDS * sizeof(char));
-		if (img->data != NULL) {
-			CLS1_SendStr((unsigned char*) "reading Bitmap file:  ", io->stdOut);
-			CLS1_SendStr((unsigned char*) fileName, io->stdOut);
+	BMPImage* image = NULL;
+	image = malloc(sizeof(BMPImage));
+	if (image != NULL) {
 
-			result = BMPImageLoad((char *) fileName, img);
+		CLS1_SendStr((unsigned char*) "reading Bitmap file:  ", io->stdOut);
+		CLS1_SendStr((unsigned char*) fileName, io->stdOut);
 
-			CLS1_SendStr((unsigned char*) "\r\n", io->stdOut);
-
-			/* ok, have now directory name */
+		result = BMPImageLoad((char *) fileName, image);
+		if (result != FR_OK) {
+			CLS1_SendStr((unsigned char*) "ERROR reading File  ",
+					CLS1_GetStdio()->stdOut);
 		} else {
-			CLS1_SendStr((unsigned char*) "reading Bitmap name failed!\r\n",
-					io->stdErr);
-			res = ERR_FAILED;
+			CLS1_SendStr((unsigned char*) "\r\n", io->stdOut);
+			CLS1_SendStr((unsigned char*) "Breite des Bitmap files:  ",
+					CLS1_GetStdio()->stdOut);
+			CLS1_SendCh(image->biWidth, CLS1_GetStdio()->stdOut);
+			CLS1_SendStr((unsigned char*) "\r\n", CLS1_GetStdio()->stdOut);
+			CLS1_SendStr((unsigned char*) "Höhe des Bitmap files:  ",
+					CLS1_GetStdio()->stdOut);
+			CLS1_SendCh(image->biHeight, CLS1_GetStdio()->stdOut);
+			CLS1_SendStr((unsigned char*) "\r\n", CLS1_GetStdio()->stdOut);
+			CLS1_SendStr((unsigned char*) "Farbtiefe in bits per pixel (Hex):  ",
+					CLS1_GetStdio()->stdOut);
+			CLS1_SendCh(image->biBitCount, CLS1_GetStdio()->stdOut);
+			CLS1_SendStr((unsigned char*) "\r\n", CLS1_GetStdio()->stdOut);
 		}
+
+		/* ok, have now directory name */
 	} else {
+		CLS1_SendStr((unsigned char*) "malloc failed!\r\n",
+				io->stdErr);
 		res = ERR_FAILED;
 	}
 
 	return res;
 }
-
 
 static uint8_t PrintHelp(const CLS1_StdIOType *io) {
 	CLS1_SendHelpStr((unsigned char*) "BMP",
@@ -154,7 +165,6 @@ static uint8_t PrintStatus(const CLS1_StdIOType *io) {
 }
 #endif
 
-
 uint8_t BMP_ParseCommand(const unsigned char *cmd, bool *handled,
 		const CLS1_StdIOType *io) {
 	if (UTIL1_strcmp((char*) cmd, CLS1_CMD_HELP) == 0
@@ -179,7 +189,7 @@ unsigned int endianReadInt(FILE* file) {
 		return 0;
 	/* LITTLE VS BIG ENDIAN LINES - comment out one or the other */
 	i = (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0]; // big endian
-	//i = (b[0]<<24) | (b[1]<<16) | (b[2]<<8) | b[3]; // little endian
+//i = (b[0]<<24) | (b[1]<<16) | (b[2]<<8) | b[3]; // little endian
 	return i;
 }
 
@@ -193,7 +203,7 @@ unsigned short int endianReadShort(FILE* file) {
 		return 0;
 	/* LITTLE VS BIG ENDIAN LINES - comment out one or the other */
 	s = (b[1] << 8) | b[0]; // big endian
-	//s = (b[0]<<8) | b[1]; // little endian
+//s = (b[0]<<8) | b[1]; // little endian
 	return s;
 }
 
@@ -206,12 +216,11 @@ int BMPImageLoad(char* filename, BMPImage* image) {
 	FILE *filee;
 	unsigned long size;                 // size of the image in bytes.
 	unsigned long i;                    // standard counter.
-	unsigned short int planes;     // number of planes in image (must be 1)
-	unsigned short int bpp;         // number of bits per pixel (must be 24)
-	char temp;            // temporary color storage for bgr-rgb conversion.
+	unsigned short int planes;     		// number of planes in image (must be 1)
+	unsigned short int bpp;         	// number of bits per pixel (must be 24)
+	char temp;            	// temporary color storage for bgr-rgb conversion.
 	FIL* file = NULL;
 	FRESULT res = FR_OK;
-	BMPImage img;
 	file = &bmpFile;
 
 #if 0
@@ -240,103 +249,51 @@ int BMPImageLoad(char* filename, BMPImage* image) {
 
 		UINT br; /* Pointer to number of bytes read */
 
-		uint8_t buf[330];
+		uint8_t buf[50];
 
 		res = FAT1_read(file, buf, sizeof(buf) - 1, &nof);
 		if (res != FR_OK) {
 			CLS1_SendStr((unsigned char*) "Error reading BMP file ",
 					CLS1_GetStdio()->stdOut);
-
 			//error occured
 		} else {
 			buf[nof] = '\0'; /* terminate buffer */
-			img.sizeX = buf[18];
-			img.sizeY = buf[22];
-			uint8_t width = buf[18];
-			uint8_t height = buf[22];
-			bfOffBits = buf[10];											// number of Offsetbits until first Colorvalue
-			CLS1_SendStr((unsigned char*) "Breite des Bitmap files:  ",
-					CLS1_GetStdio()->stdOut);
-			CLS1_SendCh(img.sizeX, CLS1_GetStdio()->stdOut);
-
-			CLS1_SendStr((unsigned char*) "\r\n", CLS1_GetStdio()->stdOut);
-			CLS1_SendStr((unsigned char*) "Höhe des Bitmap files:  ",
-					CLS1_GetStdio()->stdOut);
-			CLS1_SendCh(img.sizeY, CLS1_GetStdio()->stdOut);
-
+			image->biWidth = buf[18];
+			image->biHeight = buf[22];
+			image->bfOffBits = buf[10];
+			image->biBitCount = buf[28];
+			image->biSizeImage = buf[34];
 
 		}
 
+		image->data = malloc(
+				image->biWidth * image->biHeight * sizeof(char) * 3); // FREE nicht vergessen !!!!!!!!!!!!!!!!!
+		if (image->data != NULL) {
+			res = FAT1_lseek(file, image->bfOffBits); // filepointer wird an den Ort verschoben wo die Bilddaten beginnen
+			if (res == FR_OK) {
+				res = FAT1_read(file, image->data, sizeof(buf) - 1, &nof);	//
+				if (res == FR_OK) {
+					res = FAT1_close(file);
+				} else {
+					res = FR_DENIED; 	// close failed
+					return res;
+				}
+			} else {
+				res = FR_NOT_ENABLED; 	// seek failed
+				return res;
+			}
+		} else {
+			res = FR_NOT_ENOUGH_CORE;	// malloc failed
+			return res;
+		}
 	}
-
-// make sure the file is there.
-	if ((filee = fopen(filename, "rb")) == NULL) {
-		printf("File Not Found : %s\n", filename);
-		return 0;
-	}
-
-// seek through the bmp header, up to the width/height:
-	fseek(filee, 18, SEEK_CUR);
-
-// read the width
-	if (!(image->sizeX = endianReadInt(filee))) {
-		printf("Error reading width from %s.\n", filename);
-		return 0;
-	}
-	printf("Width of %s: %lu\n", filename, image->sizeX);
-
-// read the height
-	if (!(image->sizeY = endianReadInt(filee))) {
-		printf("Error reading height from %s.\n", filename);
-		return 0;
-	}
-	printf("Height of %s: %lu\n", filename, image->sizeY);
-
-// calculate the size (assuming 24 bits or 3 bytes per pixel).
-	size = image->sizeX * image->sizeY * 3;
-
-// read the planes
-	if (!(planes = endianReadShort(filee))) {
-		printf("Error reading planes from %s.\n", filename);
-		return 0;
-	}
-	if (planes != 1) {
-		printf("Planes from %s is not 1: %u\n", filename, planes);
-		return 0;
-	}
-
-// read the bits per pixel
-	if (!(bpp = endianReadShort(file))) {
-		printf("Error reading bpp from %s.\n", filename);
-		return 0;
-	}
-	if (bpp != 24) {
-		printf("Bpp from %s is not 24: %u\n", filename, bpp);
-		return 0;
-	}
-
-// seek past the rest of the bitmap header.
-	fseek(file, 24, SEEK_CUR);
-
-// read the data.
-	image->data = (char *) malloc(size);
-	if (image->data == NULL) {
-		printf("Error allocating memory for color-corrected image data");
-		return 0;
-	}
-
-	if ((i = fread(image->data, size, 1, file)) != 1) {
-		printf("Error reading image data from %s.\n", filename);
-		return 0;
-	}
-
+	size = ((image->biWidth) * (image->biHeight) * 3);
 	for (i = 0; i < size; i += 3) { // reverse all of the colors. (bgr -> rgb)
 		temp = image->data[i];
 		image->data[i] = image->data[i + 2];
 		image->data[i + 2] = temp;
 	}
 
-// we're done.
-	return 1;
+	return res;
 }
 
